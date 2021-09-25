a63b12b69aa44cce1cd947f8fd6adefca18d63fb
alain.plana <alain.plana@free-now.com>
2021-09-25 19:17:19 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FunctionComponent, useEffect, useState } from 'react';
+import { Person } from './types';
+import data from './data';
+import { FaQuoteRight } from 'react-icons/fa';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-function App() {
+const peopleInitialState: Person[] = data;
+const verifyIndex = (idx: number, max: number) => {
+  if (idx > max) {
+    idx = 0;
+  }
+  if (idx < 0) {
+    idx = max;
+  }
+  return idx;
+}
+
+const App: FunctionComponent = () => {
+  const [people] = useState(peopleInitialState);
+  const [index, setIndex] = useState(0);
+
+  const nextSlide = () => {
+    setIndex((old) => {
+      let index = old + 1;
+      return verifyIndex(index, people.length - 1);
+    });
+  }
+
+  const prevSlide = () => {
+    setIndex((old) => {
+      let index = old - 1;
+      return verifyIndex(index, people.length - 1);
+    });
+  }
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex((old) => {
+        let index = old + 1;
+        return verifyIndex(index, people.length - 1);
+      });
+    }, 3000);
+    return () => clearInterval(slider)
+  }, [index, people]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          let position = 'nextSlide';
+
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+            position = 'lastSlide';
+          }
+
+          return <article key={id} className={position}>
+            <img src={image} alt={name} className="person-img" />
+            <h4>{name}</h4>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+        })}
+        <button className="prev" onClick={prevSlide}><FiChevronLeft /></button>
+        <button className="next" onClick={nextSlide}><FiChevronRight /></button>
+      </div>
+    </section>
   );
 }
 
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type Person = {
+    id: number;
+    image: string;
+    name: string;
+    title: string;
+    quote: string;
+}
